65a558e2cc9217a20e27c3c62fe0c69e620cb884
Jake <jakedfunk@hotmail.com>
2024-12-15 16:37:39 -0500

    init site layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import svelte from '@astrojs/svelte';
-import tailwind from '@astrojs/tailwind';
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), svelte(), tailwind()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), svelte(), tailwind()],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+
+  daisyui: {
+    themes: [
+      {
+        onedark: {
+          primary: "#98c379",
+          secondary: "#61afef",
+          accent: "#c678dd",
+          neutral: "#ABB2BF",
+          "base-100": "#282c34",
+          "base-200": "#2c313a",
+          "base-300": "#31363f",
+          info: "#56b6c2",
+          success: "#98C379",
+          warning: "#E5C07B",
+          error: "#E06C75",
+        },
+      },
+      "nord",
+    ],
+  },
+};
